849b6273ca1d1e222778ae6309afbff4dc06a030
Calvin Vuong <cvuong@stuy.edu>
2016-10-07 21:23:15 -0400

    file initialized

---
+++ linked_list.c
@@ -0,0 +1,19 @@
+// Calvin Vuong
+// MKS65 pd10
+// Work 2
+// 10-07-2016
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node { int i; struct node *next; };
+
+
+
+
+
+
+int main() {
+
+  return 0;
+}
